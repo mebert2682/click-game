e56d08abf6d7def74428747cef8823e0589f9681
mebert2682 <michael.ebert2682@gmail.com>
2019-05-29 11:57:37 -0400

    Updates

---
+++ precache-manifest.4f70d63e133cdeffc9d6372224145792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335818e383fb4e6e0f344a5771c6daae",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "87cb946996f4d9320794",
+    "url": "/click-game/static/css/main.2fc21c09.chunk.css"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/click-game/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "87cb946996f4d9320794",
+    "url": "/click-game/static/js/main.6f14d060.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  }
+]);
